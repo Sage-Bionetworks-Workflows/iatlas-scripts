25b6916d8b03b0cac0009ca3d0210e77a8332619
Brad Macdonald <bwm03570@gmail.com>
2023-05-10 13:53:11 -0600

    add syn as function arg

--- immune_subtype_clustering/prepare_data_sheet.py
+++ immune_subtype_clustering/prepare_data_sheet.py
@@ -12,9 +12,8 @@ def syn_login():
     return syn
 
 
-def download_data_files(parent: str):
+def download_data_files(parent: str, syn: synapseclient.Synapse):
     """downloads all TSV files in parent synapse folder from synapse id provided"""
-    syn = syn_login()
     children = syn.getChildren(parent=parent)
     syn_id_list = []
     for child in children:
@@ -70,9 +69,10 @@ def verify_export(final_df: pd.DataFrame, export_name: str):
         raise ValueError("Not all columns have identical values")
 
 
-def syn_upload(export_name: str, used_list: list, syn_location: str):
+def syn_upload(
+    export_name: str, used_list: list, syn_location: str, syn: synapseclient.Synapse
+):
     """Uploads exported data file to Synapse in provided location"""
-    syn = syn_login()
     file = File(
         export_name,
         parent=syn_location,
@@ -89,12 +89,16 @@ def syn_upload(export_name: str, used_list: list, syn_location: str):
 
 
 if __name__ == "__main__":
-    syn_id_list = download_data_files(parent="syn26535390")
+    syn = syn_login()
+    syn_id_list = download_data_files(parent="syn26535390", syn=syn)
     gene_df_list = load_data_files(data_directory="./data")
     final_df = merge_dfs(gene_df_list=gene_df_list)
     export_name = verify_export(
         final_df=final_df, export_name="immune_subtype_sample_sheet.tsv"
     )
     syn_upload(
-        export_name=export_name, used_list=syn_id_list, syn_location="syn51471781"
+        export_name=export_name,
+        used_list=syn_id_list,
+        syn_location="syn51471781",
+        syn=syn,
     )

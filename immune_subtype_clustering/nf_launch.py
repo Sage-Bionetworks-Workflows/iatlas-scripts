502fe7342de5285b1a4d96534ab82754b4b9f416
Brad Macdonald <bwm03570@gmail.com>
2023-05-11 10:32:30 -0600

    create nf_launch

---
+++ immune_subtype_clustering/nf_launch.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import sys
+
+from orca.services.nextflowtower import NextflowTowerOps
+from orca.services.nextflowtower.models import LaunchInfo
+
+from immune_subtype_clustering.utils import monitor_run, configure_logging
+
+
+def nextflow_execute_workflow(run_name: str, s3_file: str, cwl_file: str):
+    """Launches nf-synstage workflow in Nextflow Tower"""
+    ops = NextflowTowerOps()
+    info = LaunchInfo(
+        run_name=run_name,
+        pipeline="https://github.com/Sage-Bionetworks-Workflows/nf-cwl-wrap",
+        revision="main",
+        params={
+            "s3_file": s3_file,
+            "cwl_file": cwl_file,
+        },
+    )
+    run_id = ops.launch_workflow(info, "ondemand")
+    monitor_run(ops, run_id)
+
+
+def main():
+    run_name = sys.argv[1]  # "immune_subtype_clustering_execution"
+    s3_file = sys.argv[
+        2
+    ]  # "s3://iatlas-project-tower-bucket/immune_subtype_clustering/synstage/input.csv"
+    cwl_file = sys.argv[
+        3
+    ]  # "https://raw.githubusercontent.com/CRI-iAtlas/iatlas-workflows/develop/Immune_Subtype_Clustering/workflow/steps/immune_subtype_clustering/immune_subtype_clustering.cwl"
+    configure_logging()
+    nextflow_execute_workflow(run_name=run_name, s3_file=s3_file, cwl_file=cwl_file)
+
+
+if __name__ == "__main__":
+    main()

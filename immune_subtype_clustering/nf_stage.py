8b5440464d037ee2948a368fc2c85fc2d3f57f03
Brad Macdonald <bwm03570@gmail.com>
2023-05-11 10:32:24 -0600

    organize nf_stage

--- immune_subtype_clustering/nf_stage.py
+++ immune_subtype_clustering/nf_stage.py
@@ -1,66 +1,41 @@
+#!/usr/bin/env python3
+
+import sys
+
 from orca.services.nextflowtower import NextflowTowerOps
 from orca.services.nextflowtower.models import LaunchInfo
 
-
-def monitor_run(ops: NextflowTowerOps, run_id: str):
-    """Wait until workflow run completes."""
-    from time import sleep
-
-    workflow = ops.get_workflow(run_id)
-    print(f"Starting to monitor workflow: {workflow.run_name} ({run_id})")
-
-    status, is_done = ops.get_workflow_status(run_id)
-    while not is_done:
-        print(f"Not done yet. Status is '{status.value}'. Checking again in 5 min...")
-        sleep(60 * 5)
-        status, is_done = ops.get_workflow_status(run_id)
-
-    print(f"Done! Final status is '{status.value}'.")
-    return status
+from immune_subtype_clustering.utils import monitor_run, configure_logging
 
 
-def configure_logging():
-    """Configure logging for Orca and Airflow."""
-    import logging
-
-    # Remove timestamps from Orca log statements
-    logger = logging.getLogger("orca")
-    logger.propagate = False
-    logger.handlers.clear()
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-    # Silence Airflow logging
-    logging.getLogger("airflow").setLevel(logging.ERROR)
-
-
-def nextflow_stage_data():
+def nextflow_stage_data(run_name: str, input: str, outdir: str):
     """Launches nf-synstage workflow in Nextflow Tower"""
-    # Credentials are pulled from an environment variable
     ops = NextflowTowerOps()
-
-    # Define a workflow run using LaunchInfo
     info = LaunchInfo(
-        run_name="immune_subtype_clustering_staging",
+        run_name=run_name,
         pipeline="https://github.com/Sage-Bionetworks-Workflows/nf-synstage",
         revision="main",
         workspace_secrets=["SYNAPSE_AUTH_TOKEN"],
-        # Comment out to intentionally cause an error with the workflow run
         params={
-            "input": "s3://iatlas-project-tower-bucket/immune_subtype_clustering/input.csv",
-            "outdir": "s3://iatlas-project-tower-bucket/immune_subtype_clustering/",
+            "input": input,
+            "outdir": outdir,
         },
     )
-
-    # Many details are filled in automatically (e.g., compute environment)
     run_id = ops.launch_workflow(info, "ondemand")
-
-    # Monitor the current workflow run until it completes
     monitor_run(ops, run_id)
 
 
-if __name__ == "__main__":
+def main():
+    run_name = sys.argv[1]  # "immune_subtype_clustering_staging"
+    input = sys.argv[
+        2
+    ]  # "s3://iatlas-project-tower-bucket/immune_subtype_clustering/input.csv"
+    outdir = sys.argv[
+        3
+    ]  # "s3://iatlas-project-tower-bucket/immune_subtype_clustering/"
     configure_logging()
-    nextflow_stage_data()
+    nextflow_stage_data(run_name=run_name, input=input, outdir=outdir)
+
+
+if __name__ == "__main__":
+    main()
